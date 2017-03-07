93c17115155cee4e0f80279ac601f038fb1d5470
bpjoshi <bpjoshi@bpjoshi.com>
2017-03-07 18:59:54 +0530

    Towers of Hanoi Solution in Java

--- TowersOfHanoi.java
+++ TowersOfHanoi.java
@@ -1,3 +1,26 @@
 public class TowersOfHanoi{
+	public void towersOfHanoi(int n, char start, char aux, char end) {
+		
+		//If there is only one disk, shift it to end Tower
+        if (n == 1) {
+            System.out.println(start + " shifting to -> " + end);
+        } else {
+        	//Move top n-1 disks from start A to B using C as Auxiliary 
+        	towersOfHanoi(n - 1, start, end, aux);
+        	//Move the nth (only remaining) disk from start A to  destination C
+            System.out.println(start + " shifting to -> " + end);
+            //move the top n-1 disk from aux B to end C using A as Auxiliary
+            towersOfHanoi(n - 1, aux, start, end);
+        }
+}
+
+public static void main(String[] args) {
+
+	TowersOfHanoi towersOfHanoi = new TowersOfHanoi();
+
+        System.out.print("Enter number of discs: ");
+        int discs = 10;
+        towersOfHanoi.towersOfHanoi(discs, 'A', 'B', 'C');
+}
 
 }

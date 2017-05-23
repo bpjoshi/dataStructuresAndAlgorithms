7c6344db1297f87791679aab9f67f694bf7356e7
bpjoshi <bpjoshi@bpjoshi.com>
2017-05-23 13:16:43 +0530

    Doubly linked list

---
+++ DoublyLinkedList/DoublyLinkedListAddDelete.java
@@ -0,0 +1,44 @@
+/**
+ * @author Bhagwati Prasad(bpjoshi)
+ */
+public class DoublyLinkedListAddDelete {
+	
+	static class Node
+	   {
+	      String name;
+	      Node next;
+	      Node previous;
+	   }
+	
+	public static void main(String[] args) {
+		//Generating a doubly linked list
+		  Node topForward = new Node ();
+	      topForward.name = "A";
+	      Node temp = new Node ();
+	      temp.name = "B";
+	      Node topBackward = new Node ();
+	      topBackward.name = "C";
+	      topForward.next = temp;
+	      temp.next = topBackward;
+	      topBackward.next = null;
+	      topBackward.previous = temp;
+	      temp.previous = topForward;
+	      topForward.previous = null;
+	      //Print forward singly linkedList
+	      temp=topForward;
+	      System.out.print("Forward Singly Linked List: ");
+	      while(temp!=null){
+	    	  System.out.print(temp.name+" ");
+	    	  temp=temp.next;
+	      }
+	     System.out.println();
+	     //Print backward singly linked list
+	     temp=topBackward;
+	      System.out.print("Backward Singly Linked List: ");
+	      while(temp!=null){
+	    	  System.out.print(temp.name+" ");
+	    	  temp=temp.previous;
+	      }
+	     System.out.println();
+	}
+}

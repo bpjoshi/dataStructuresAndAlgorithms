e5d558942d1331f2f2910eadde20d7654a957a93
bpjoshi <bpjoshi@bpjoshi.com>
2017-05-24 16:19:16 +0530

    Stack Interface will be implemented by Stack Implementation

---
+++ Stacks/StackInterface.java
@@ -0,0 +1,10 @@
+package stacks;
+/**
+ * @author Bhagwati Prasad(bpjoshi)
+ */
+public interface StackInterface {
+	 boolean isEmpty ();
+	 Object peek ();
+	 void push (Object o);
+	 Object pop ();
+}
